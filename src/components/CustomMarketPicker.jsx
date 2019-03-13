7f5fb479626104319e8360418268a81be5835afc
little-wave <little-wave@lobstr.co>
2019-03-13 18:15:46 +0300

    CustomMarketPicker refactored

--- src/components/CustomMarketPicker.jsx
+++ src/components/CustomMarketPicker.jsx
@@ -1,59 +1,59 @@
-const React = window.React = require('react');
-import AssetPair from './AssetPair.jsx';
-import AssetPickerNarrow from './AssetPickerNarrow.jsx';
-import Stellarify from '../lib/Stellarify';
-import _ from 'lodash';
+import React from 'react';
+import AssetPair from './AssetPair';
+import AssetPickerNarrow from './AssetPickerNarrow';
 
 export default class CustomMarketPicker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      baseBuying: null,
-      counterSelling: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            baseBuying: null,
+            counterSelling: null,
+        };
+
+        this.baseBuyingUpdate = (asset) => {
+            this.setState({
+                baseBuying: asset,
+            });
+        };
+        this.counterSellingUpdate = (asset) => {
+            this.setState({
+                counterSelling: asset,
+            });
+        };
     }
 
-    this.baseBuyingUpdate = (asset) => {
-      this.setState({
-        baseBuying: asset,
-      });
-    }
-    this.counterSellingUpdate = (asset) => {
-      this.setState({
-        counterSelling: asset,
-      });
-    }
-  }
-  render() {
-    let results;
-    if (this.state.baseBuying || this.state.counterSelling) {
-      results = <div className="CustomMarketPicker__results">
-        <AssetPair row={true} baseBuying={this.state.baseBuying} counterSelling={this.state.counterSelling}></AssetPair>
-      </div>
-    }
+    render() {
+        const { baseBuying, counterSelling } = this.state;
+        const classDivision = 'island__sub__division';
 
-    return (
-      <div className="island">
-        <div className="island__header">
-          Custom exchange pair
-        </div>
-        <div className="island__sub CustomMarketPicker__title">
-          <div className="island__sub__division">
-            <h3 className="island__sub__division__title">Base asset</h3>
-          </div>
-          <div className="island__sub__division">
-            <h3 className="island__sub__division__title">Counter asset</h3>
-          </div>
-        </div>
-        <div className="island__sub CustomMarketPicker__picker">
-          <div className="island__sub__division">
-            <AssetPickerNarrow onUpdate={this.baseBuyingUpdate}></AssetPickerNarrow>
-          </div>
-          <div className="island__sub__division">
-            <AssetPickerNarrow onUpdate={this.counterSellingUpdate}></AssetPickerNarrow>
-          </div>
-        </div>
-        {results}
-      </div>
-    );
-  }
-};
+        const pickedAsset =
+            baseBuying || counterSelling ? (
+                <AssetPair row baseBuying={this.state.baseBuying} counterSelling={this.state.counterSelling} />
+            ) : null;
+
+        return (
+            <div className="island">
+                <div className="island__header">Custom exchange pair</div>
+
+                <div className="island__sub CustomMarketPicker_title">
+                    <div className={classDivision}>
+                        <h3 className={`${classDivision}__title`}>Base asset</h3>
+                    </div>
+                    <div className={classDivision}>
+                        <h3 className={`${classDivision}__title`}>Counter asset</h3>
+                    </div>
+                </div>
+
+                <div className="island__sub CustomMarketPicker_picker">
+                    <div className={classDivision}>
+                        <AssetPickerNarrow onUpdate={this.baseBuyingUpdate} />
+                    </div>
+                    <div className={classDivision}>
+                        <AssetPickerNarrow onUpdate={this.counterSellingUpdate} />
+                    </div>
+                </div>
+                {pickedAsset}
+            </div>
+        );
+    }
+}
